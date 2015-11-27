2b9afbf20910a93c3caaa1548e10af6774bfe247
opot <youtolife@yandex.ru>
2015-11-27 20:52:13 +0300

    Refactoring, using stack and DateTime

--- Brain/Field.cs
+++ Brain/Field.cs
@@ -6,39 +6,31 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Brain {
-	struct Field {
-		CellState[,] cells;
+	class Field {
+		CellState[][] cells;
 
-		public Field(String folder,CellState player) {
+		public Field(String folder) {
 
-			cells = new CellState[10,10];
-			for (int i = 0; i < 10; i++) {
-				for (int j = 0; j < 10; j++)
-					cells[i,j] = CellState.Empty;
+			cells = new CellState[10][];
+			for (int i = 0; i < 10; i++)
+				cells[i] = new CellState[10];
+		
+			cells[4][9] = CellState.Block;
+			cells[5][9] = CellState.Block;
+
+			DebugOutput();
+			String[] moves = Directory.GetFiles(folder);
+
+			for(int i = 0; i < moves.Length/2; i++) {
+				String s = File.ReadAllLines(moves[i])[0];
+				addNoChangeSymb(CellState.Cross, Convert.ToInt32(s));
+
+				s = File.ReadAllLines(moves[i+1])[0];
+				addNoChangeSymb(CellState.Zero, Convert.ToInt32(s));
 			}
-			cells[9,4] = CellState.Block;
-			cells[9,5] = CellState.Block;
-
-			String[] xMoves;
-			String[] oMoves;
-			if (player == CellState.Cross) {
-				xMoves = Directory.GetFiles(folder);
-				oMoves = Directory.GetFiles(folder + "o");
-			} else {
-				oMoves = Directory.GetFiles(folder);
-				xMoves = Directory.GetFiles(folder + "x");
-			}
-
-			for(int i = 0; i < oMoves.Length; i++) {
-				String s = File.ReadAllLines(xMoves[i])[0];
-				addSymb(CellState.Cross, Convert.ToInt32(s));
-
-				s = File.ReadAllLines(oMoves[i])[0];
-				addSymb(CellState.Zero, Convert.ToInt32(s));
-			}
-			if(player == CellState.Zero) {
-				String s = File.ReadAllLines(xMoves[xMoves.Length-1])[0];
-				addSymb(CellState.Cross, Convert.ToInt32(s));
+			if(moves.Length % 2 == 1) {
+				String s = File.ReadAllLines(moves[moves.Length-1])[0];
+				addNoChangeSymb(CellState.Cross, Convert.ToInt32(s));
 			}
 
 			Console.WriteLine(" ");
@@ -46,59 +38,70 @@ namespace Brain {
 
 		}
 
+		private void addNoChangeSymb(CellState player, int row) {
+			if (row != 4 && row != 5) {
+				for (int i = 0; i < 9; i++)
+					cells[row][i] = cells[row][i + 1];
+				cells[row][9] = player;
+			} else {
+				for (int i = 0; i < 8; i++)
+					cells[row][i] = cells[row][i + 1];
+				cells[row][8] = player;
+			}
+		}
+
 		public Field(Field last, int row, CellState player) {
-			cells = new CellState[10,10];
+			cells = new CellState[10][];
 			for (int i = 0; i < 10; i++)
-				for (int j = 0; j < 10; j++)
-					cells[i, j] = last.cells[i, j];
-			this.addSymb(player, row);
+				cells[i] = last.cells[i];
+
+			cells[row] = new CellState[10];
+			for (int i = 1; i < 9; i++)
+				cells[row][i] = last.cells[row][i-1];
+			cells[row][9] = player;
+		}
+
+		public bool checkRow(int row) {
+			return (cells[row][0] == CellState.Empty);
 		}
 
 		private void addSymb(CellState player, int row) {
 			if (row != 4 && row != 5) {
 				for (int i = 1; i <= 9; i++)
-					cells[i - 1, row] = cells[i, row];
-				cells[9, row] = player;
+					cells[row][i - 1] = cells[row][i];
+				cells[row][9] = player;
 			} else {
 				for (int i = 1; i <= 8; i++)
-					cells[i - 1, row] = cells[i, row];
-				cells[8, row] = player;
+					cells[row][i - 1] = cells[row][i];
+				cells[row][8] = player;
 			}
-
-			//	DebugOutput();
-
 		}
 
-		public bool checkRow(int row) {
-			return (cells[0, row] == CellState.Empty);
-		}
+		public CellState check(int row) {
 
-		public CellState check() {
 			for (int line = 0; line < 9; line++) {
 				for (int i = 0; i < 5; i++) {
-					if (cells[line, i] == CellState.Cross || cells[line, i] == CellState.Zero) {
+					if (cells[i][line] == CellState.Cross || cells[i][line] == CellState.Zero) {
 						bool isAll = true;
 						for (int j = i + 1; j < i + 5; j++)
-							if (cells[line, i] != cells[line, j])
+							if (cells[i][line] != cells[j][line])
 								isAll = false;
 						if (isAll)
-							return cells[line, i];
+							return cells[i][line];
 					}
 				}
 			}
 
-			for (int row = 0; row < 9; row++) {
-				for (int i = 0; i < 5; i++) {
-					if (cells[i, row] == CellState.Cross || cells[i, row] == CellState.Zero) {
-						bool isAll = true;
-						for (int j = i + 1; j < i + 5; j++)
-							if (cells[i, row] != cells[j, row])
-								isAll = false;
-						if (isAll)
-							return cells[row, i];
-					}
-				}
-			}
+			int delta = 0;
+			if(row == 4 || row == 5)
+				delta = 1;
+
+				if (cells[row][delta] != CellState.Empty)
+					if (cells[row][delta] == cells[row][delta + 1] &&
+					   cells[row][delta] == cells[row][delta + 2] &&
+					   cells[row][delta] == cells[row][delta + 3] &&
+					   cells[row][delta] == cells[row][delta + 4])
+						return cells[row][delta];
 
 			/*for (int diag = 0; diag <= 4; diag++) {
 				int max_streak = 0;
@@ -113,16 +116,103 @@ namespace Brain {
 		public void DebugOutput() {
 			for (int i = 0; i < 10; i++) {
 				for (int j = 0; j < 10; j++) {
-					if (cells[i, j] == CellState.Cross)
+					if (cells[j][i] == CellState.Cross)
 						Console.Write("x");
-					if (cells[i, j] == CellState.Zero)
+					if (cells[j][i] == CellState.Zero)
 						Console.Write("O");
-					if (cells[i, j] == CellState.Empty)
+					if (cells[j][i] == CellState.Empty)
 						Console.Write("_");
-					if (cells[i, j] == CellState.Block)
+					if (cells[j][i] == CellState.Block)
 						Console.Write("=");
 				}
 				Console.WriteLine();
+			}
+
+		}
+
+		public class Solvation {
+			Field field;
+			Solvation last;
+
+			CellState player;
+
+			int row = 0;
+
+			int wins = 0;
+			int loses = 0;
+
+			float mathWait = 0;
+
+			private bool canMove = true;
+			private bool Finaized = false;
+			public bool isFinalized {
+				get {
+					return Finaized;
+				}
+			} 
+
+			public Solvation(Field field, CellState player) {
+				this.field = field;
+				this.player = player;
+				last = null;
+			}
+
+			public Solvation(Solvation last, int row){
+				this.row = row;
+				this.player = invertCell(last.player);
+				if (!last.field.checkRow(row)) {
+					Finaized = true;
+					canMove = false;
+					return;
+				}
+
+				field = new Field(last.field, row, player);
+				this.last = last;
+				CellState res = field.check(row);
+				if (res != CellState.Empty) {
+					wins += player == res ? 1 : 0;
+					loses += player == res ? 0 : 1;
+					Finaized = true;
+					mathWait = player == res? 1:-1;
+					last.toStart(loses, wins, mathWait);
+				}
+			}
+
+			void toStart(int wins, int loses, float mathWait) {
+				this.wins += wins;
+				this.loses += loses;
+				this.mathWait -= mathWait * 0.1f;
+			}
+
+			private CellState invertCell(CellState player) {
+				return (player == CellState.Cross) ? CellState.Zero : CellState.Cross;
+			}
+
+			public string getTurn() {
+				return row.ToString();
+			}
+
+			public Solvation isGreater(Solvation other) {
+				if(this.canMove && other.canMove) {
+					if (mathWait > other.mathWait)
+						return this;
+					if (mathWait < other.mathWait)
+						return other;
+
+					if (wins > other.wins)
+						return this;
+					if (wins < other.wins)
+						return other;
+
+					if (loses > other.loses)
+						return this;
+					if (loses < other.loses)
+						return other;
+
+					return this;
+				} else {
+					return canMove?this:other;
+				}
 			}
 
 		}
--- Brain/Program.cs
+++ Brain/Program.cs
@@ -1,101 +1,71 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Solvation = Brain.Field.Solvation;
 
 namespace Brain {
 
 	public enum CellState {
-		Cross, Zero, Empty, Block
+		Empty, Cross, Zero, Block
 	}
 
-	struct SolveResult{
-		public int row;
-		public int wins;
-		public int loses;
+	sealed class Program {
 
-		public SolveResult(int row, int wins, int loses) {
-			this.row = row;
-			this.wins = wins;
-			this.loses = loses;
-		}
+		DateTime start = DateTime.Now;
+		CellState player;
+		int time;
 
-		public bool CompareTo(SolveResult a) {
-			if (loses > a.loses)
-				return true;
-			if (loses < a.loses)
-				return false;
+		int calcField = 1;
+		int nextCycleLength = 0;
+		int curCycleLength = 1;
+		Queue<Solvation> que = new Queue<Solvation>();
 
-			if (wins < a.wins)
-				return true;
+		Solvation[] branch = new Solvation[10];
 
-			return false;
-		}
-	}
+		public Program(string fold, CellState player, int time) {
+			this.player = player;
+			this.time = time;
 
-	public sealed class Program {
+			Field field = new Field(fold);
+			Solve(new Solvation(field, player));
+			que.CopyTo(branch, 0);
 
-		public Program(string fold, CellState player) {
+			while (isinTime())
+				for (int i = 0; i < curCycleLength; i++)
+					Solve(que.Dequeue());
 
-			Field field = new Field(fold, player);
+			Solvation max = branch[0];
+			for (int i = 1; i < 10; i++)
+				max = branch[i].isGreater(max);
 
-			List<SolveResult> result = new List<SolveResult>(10);
-
-			for (int i = 0; i < 10; i++) {
-				if (field.checkRow(i)) {
-					SolveResult cur = makeTurn(invertCell(player), 4, new Field(field, i, player), i);
-					result.Add(cur);
-				}
-			}
-
-			SolveResult min = result[0];
-			for (int i = 1; i < result.Count; i++)
-				if (min.CompareTo(result[i]))
-					min = result[i];
 
 			int turn = Directory.GetFiles(fold).Length + 1;
 			string path = fold + (player == CellState.Cross ? "x" : "o") + turn.ToString() + ".txt";
-			File.WriteAllLines(path, new String[] { min.row.ToString() });
+			File.WriteAllLines(path, new String[] { max.getTurn() });
 		}
 
-		private SolveResult makeTurn(CellState player, int deep, Field field, int lastRow) {
-			CellState field_state = field.check();
-
-			if (field_state != CellState.Empty)
-				return new SolveResult(lastRow, field_state == player ? 1 : 0, field_state == player ? 0 : 1);
-
-			if (deep == 0)
-				return new SolveResult(lastRow, 0, 0);
-
-			List<SolveResult> result = new List<SolveResult>(10);
-			SolveResult thisResult = new SolveResult(lastRow, 0, 0);
-
-			for (int i = 0; i < 10; i++) {
-				if (field.checkRow(i)) {
-					SolveResult cur = makeTurn(invertCell(player), deep - 1, new Field(field, i, player), i);
-					thisResult.wins += cur.loses;
-					thisResult.loses += cur.wins;
-					result.Add(cur);
+		void Solve(Solvation solve) {
+			for(int i = 0; i < 10; i++) {
+				Solvation buf = new Solvation(solve, i);
+				if (!buf.isFinalized) {
+					que.Enqueue(buf);
+					nextCycleLength++;
+					calcField++;
 				}
 			}
-
-			SolveResult min = result[0];
-			for (int i = 1; i < result.Count; i++)
-				if (min.CompareTo(result[i]))
-					min = result[i];
-			//thisResult.row = min.row;
-			return thisResult;
 		}
 
-		public CellState invertCell(CellState player) {
-			return (player == CellState.Cross) ? CellState.Zero : CellState.Cross;
-        }
+		bool isinTime() {
+			int d = (DateTime.Now.Second - start.Second) * 1000 + (DateTime.Now.Millisecond - start.Millisecond);
+			float needtime = (d / (float)calcField)*nextCycleLength*1.1f + 100;
+			Console.WriteLine(d);
+			return needtime <= time;
+		}
 
 		static void Main(string[] args) {
-			new Program(args[0], args[2][0] == 'x'?CellState.Cross: CellState.Zero);
-			//Console.ReadKey();
+			new Program(args[0], args[2][0] == 'x'?CellState.Cross: CellState.Zero, Convert.ToInt32(args[2]));
+			Console.ReadKey();
 		}
 	}
 
